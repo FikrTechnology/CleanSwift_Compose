43e33518c11d3a13e2769ce45b7e2e22db41e465
rdl-rcd <rdl-rcd@bni.co.id>
2025-04-23 16:19:07 +0700

    first commit

---
+++ app/src/main/java/com/example/cleanswift/presentation/SplashScreen.kt
@@ -0,0 +1,99 @@
+package com.example.cleanswift.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cleanswift.MainActivity
+import com.example.cleanswift.R
+import kotlinx.coroutines.delay
+
+
+class SplashScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        setContent {
+            SplashScreenActivity()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun SplashScreenActivity(){
+        val alpha = remember {
+            Animatable(0f)
+        }
+
+        LaunchedEffect(key1 = true) {
+            alpha.animateTo(1f,
+                animationSpec = tween(1500)
+            )
+            delay(2000)
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            })
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFFB7E0FF))
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo_splash),
+                    contentDescription = "Logo Splash"
+                )
+
+                Text(
+                    text = "CleanSwift",
+                    modifier = Modifier.padding(top = 16.dp),
+                    color = Color.Black,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    text = "Fast & Fresh Laundry at Your Fingertips",
+                    modifier = Modifier.padding(top =20.dp),
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
