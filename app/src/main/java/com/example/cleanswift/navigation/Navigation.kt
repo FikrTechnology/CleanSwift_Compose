6634ec49694357fcc539062145aae20e7357d5da
rdl-rcd <rdl-rcd@bni.co.id>
2025-04-24 08:29:25 +0700

    Adding Navigation Route

---
+++ app/src/main/java/com/example/cleanswift/navigation/Navigation.kt
@@ -0,0 +1,21 @@
+package com.example.cleanswift.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cleanswift.presentation.login.LoginScreen
+
+@Composable
+fun SetupNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+        composable(route = Screen.Login.route) {
+            LoginScreen(navController)
+        }
+    }
+}
